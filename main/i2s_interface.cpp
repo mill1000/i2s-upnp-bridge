c5c25e3f73c9cacf07afac8904a4c08ab5c9cd05
Tucker Kern <tuckkern@gmail.com>
2020-12-16 20:37:20 -0700

    Update I2S interface for ESP32

--- main/i2s_interface.cpp
+++ main/i2s_interface.cpp
@@ -1,7 +1,10 @@
 #include <string.h>
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "driver/i2s.h"
+#include "driver/gpio.h"
+#include "esp_task_wdt.h"
 #include "esp_log.h"
 #include "esp_err.h"
 
@@ -11,42 +14,39 @@
 
 void I2S::init()
 {
-  i2s_config_t config;
-  memset(&config, 0, sizeof(i2s_config_t));
-
-  config.mode = (i2s_mode_t) (I2S_MODE_SLAVE | I2S_MODE_RX);
-  config.sample_rate = 48e3;
-  config.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT;
-  config.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT;
-  config.communication_format = (i2s_comm_format_t) (I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB);
-  config.dma_buf_count = 8;
-  config.dma_buf_len = 256;
-
-  // Configre the I2S driver
-  i2s_driver_install(I2S_NUM_0, &config, 0, NULL);
-
   i2s_pin_config_t pin_config;
   memset(&pin_config, 0, sizeof(i2s_pin_config_t));
 
   // Enable inputs for RX mode
-  pin_config.bck_i_en = true;
-  pin_config.ws_i_en = true;
-  pin_config.data_in_en = true;
+  pin_config.bck_io_num = GPIO_NUM_14;
+  pin_config.ws_io_num = GPIO_NUM_12;
+  pin_config.data_in_num = GPIO_NUM_27;
 
   // Enable pins
   i2s_set_pin(I2S_NUM_0, &pin_config);
 
-  int16_t samples[4];
-  while(1)
-  {
-    size_t read = 0;
-    esp_err_t result = i2s_read(I2S_NUM_0, samples, 4, &read, portMAX_DELAY);
-    if (result != ESP_OK)
-    {
-      ESP_LOGW(TAG, "Failed to read I2S samples.");
-      continue;
-    }
+  i2s_config_t config;
+  memset(&config, 0, sizeof(i2s_config_t));
 
-    ESP_LOGI(TAG, "Read samples %d %d", samples[0], samples[1]);
-  }
+  config.mode = (i2s_mode_t) (I2S_MODE_SLAVE | I2S_MODE_RX);
+  config.sample_rate = I2S::SAMPLE_FREQUENCTY;
+  config.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT;
+  config.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT;
+  config.communication_format = I2S_COMM_FORMAT_STAND_MSB;
+  config.dma_buf_count = I2S::BUFFER_COUNT;
+  config.dma_buf_len = I2S::BUFFER_SAMPLE_COUNT;
+  config.use_apll = true; // Use the better lock
+  config.fixed_mclk = 0; // Use automatic dividers
+  config.intr_alloc_flags = 0; // ??
+
+  // Configre the I2S driver
+  i2s_driver_install(I2S_NUM_0, &config, 0, NULL);
+}
+
+size_t I2S::read(sample_t samples[], size_t length, TickType_t waitTicks)
+{
+  size_t read = 0;
+  i2s_read(I2S_NUM_0, samples, length, &read, waitTicks);
+
+  return read;
 }
--- main/i2s_interface.h
+++ main/i2s_interface.h
@@ -1,9 +1,21 @@
 #ifndef __I2S_INTERFACE_H__
 #define __I2S_INTERFACE_H__
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 namespace I2S
 {
+  constexpr int SAMPLE_FREQUENCTY = 48e3;
+
+  // Total buffer space = 50 * 480 * 4 bytes -> 96 kB
+  constexpr int BUFFER_SAMPLE_COUNT = 480;  // Number of samples to make a 10 ms chunk @ 48 kHz
+  constexpr int BUFFER_COUNT = 50;  // 50 * 10 ms -> 500 ms of buffering
+
+  typedef int16_t sample_t;
+
   void init(void);
+  size_t read(sample_t samples[], size_t length = BUFFER_SAMPLE_COUNT * sizeof(sample_t), TickType_t waitTicks = portMAX_DELAY);
 }
 
 #endif
