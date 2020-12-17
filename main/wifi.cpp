db76c59c0538532a8ba3036b6ef4f38111ba14e1
Tucker Kern <tuckkern@gmail.com>
2020-12-16 19:16:17 -0700

    Convert project to ESP32. Inital tests on ESP8266 seem to indicate I2S will not work correctly

--- main/wifi.cpp
+++ main/wifi.cpp
@@ -1,7 +1,6 @@
 #include "esp_wifi.h"
 #include "esp_err.h"
 #include "esp_log.h"
-#include "esp_event_loop.h"
 
 #include <cstring>
 #include <string>
@@ -13,19 +12,24 @@
 static uint32_t retry_count = WiFi::RETRY_COUNT;
 
 /**
-  @brief  Event handler for WiFi
+  @brief  Event handler for WIFI_EVENT base
 
   @param  arg Argument supplied when registering handler
-  @param  event system_event_t of event
-  @retval esp_err_t
+  @param  event_base esp_event_base_t of event
+  @param  event_id Event ID of generated event
+  @param  event_data Pointer to event data structure
+  @retval none
 */
-static esp_err_t wifiEventHandler(void* arg, system_event_t* event)
+static void wifiEventHandler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
-  switch (event->event_id)
+  // We should only get WIFI_EVENTs in this handler
+  assert(event_base == WIFI_EVENT);
+
+  switch (event_id)
   {
-    case SYSTEM_EVENT_STA_START:
+    case WIFI_EVENT_STA_START:
     {
-      // // Set hostname before connection
+      // Set hostname before connection
       // std::string hostname = NVS::get_hostname();
       // if (!hostname.empty())
       // {
@@ -36,12 +40,13 @@ static esp_err_t wifiEventHandler(void* arg, system_event_t* event)
       esp_wifi_connect();
       break;
     }
+     
 
-    case SYSTEM_EVENT_STA_CONNECTED:
+    case WIFI_EVENT_STA_CONNECTED:
       ESP_LOGI(TAG, "Connected.");
       break;
 
-    case SYSTEM_EVENT_STA_DISCONNECTED:
+    case WIFI_EVENT_STA_DISCONNECTED:
     {
       if (retry_count--) 
       {
@@ -52,12 +57,34 @@ static esp_err_t wifiEventHandler(void* arg, system_event_t* event)
         ESP_LOGE(TAG, "Failed to connect.");
       
       break;
-    }
+    } 
+    
+    default:
+      ESP_LOGW(TAG, "Unhandled event. Base: '%s' ID: %d.", event_base, event_id);
+      break;
+  }
+}
 
-     case SYSTEM_EVENT_STA_GOT_IP:
+/**
+  @brief  Event handler for IP_EVENT base
+
+  @param  arg Argument supplied when registering handler
+  @param  event_base esp_event_base_t of event
+  @param  event_id Event ID of generated event
+  @param  event_data Pointer to event data structure
+  @retval none
+*/
+static void ipEventHandler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+  // We should only get IP_EVENTs in this handler
+  assert(event_base == IP_EVENT);
+  
+  switch (event_id)
+  {
+    case IP_EVENT_STA_GOT_IP:
     {
-      system_event_sta_got_ip_t* got_ip = (system_event_sta_got_ip_t*) &event->event_info.got_ip;
-      ESP_LOGI(TAG, "Assigned IP: %s", ip4addr_ntoa(&got_ip->ip_info.ip));
+      ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+      ESP_LOGI(TAG, "Assigned IP: %s", ip4addr_ntoa(&event->ip_info.ip));
       
       // Reset retires
       retry_count = WiFi::RETRY_COUNT;
@@ -65,13 +92,10 @@ static esp_err_t wifiEventHandler(void* arg, system_event_t* event)
       break;
     }
 
-    
     default:
-      ESP_LOGW(TAG, "Unhandled event. ID: %d.", event->event_id);
-      break;
+      ESP_LOGW(TAG, "Unhandled event. Base: '%s' ID: %d.", event_base, event_id);
+    break;
   }
-
-  return ESP_OK;
 }
 
 /**
@@ -84,10 +108,13 @@ void WiFi::init_station()
 {
   tcpip_adapter_init();
 
-  ESP_ERROR_CHECK(esp_event_loop_init(&wifiEventHandler, NULL));
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
 
   wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&config));
+
+  ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifiEventHandler, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ipEventHandler, NULL));
 
   wifi_config_t wifi_config;
   memset(&wifi_config, 0, sizeof(wifi_config_t));
@@ -96,8 +123,13 @@ void WiFi::init_station()
   strcpy((char*)wifi_config.sta.ssid,     CONFIG_WIFI_SSID);
   strcpy((char*)wifi_config.sta.password, CONFIG_WIFI_PASSWORD);
 
+ 
+  // Enable protected management frames
+  wifi_config.sta.pmf_cfg.capable = true;
+  wifi_config.sta.pmf_cfg.required = false;
+
   // Require WPA2
-  wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+  //wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
 
   ESP_LOGI(TAG, "Connecting to SSID '%s'...", wifi_config.sta.ssid);
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
