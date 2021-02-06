e1c4139583e133f65b9f4be6c47f778ab2f3b014
Tucker Kern <tuckkern@gmail.com>
2021-02-05 20:05:25 -0700

    Enable automatic light sleep when configured

--- main/main.cpp
+++ main/main.cpp
@@ -3,6 +3,7 @@
 #include "freertos/timers.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "esp_pm.h"
 
 #include "http.h"
 #include "nvs_interface.h"
@@ -37,6 +38,16 @@ extern "C" void app_main()
 
   // Initialize I2S Rx
   I2S::init();
+
+#ifdef CONFIG_ENABLE_AUTOMATIC_LIGHT_SLEEP
+  esp_pm_config_esp32_t config;
+  config.max_freq_mhz = CONFIG_ESP32_DEFAULT_CPU_FREQ_MHZ;
+  config.min_freq_mhz = rtc_clk_xtal_freq_get();
+  config.light_sleep_enable = true;
+
+  // Reconfigure power management to enable automatic light sleep.
+  esp_pm_configure(&config);
+#endif
 
   // Start the HTTP task
   xTaskCreate(HTTP::task, "HTTPTask", 8192, NULL, 4, NULL);
