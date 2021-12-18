493681740b2933e36e07959e65d683c808073f59
Tucker Kern <tuckkern@gmail.com>
2021-12-18 09:48:07 -0700

    Minimally covert HTTP to Mongoose v7 API. - OTA disabled, streams disabled,

--- main/http.cpp
+++ main/http.cpp
@@ -25,17 +25,17 @@ static SemaphoreHandle_t client_mutex;
   @param  nc Mongoose connection
   @param  ev Mongoose event calling the function
   @param  ev_data Event data pointer
-  @param  user_data User data pointer
+  @param  fn_data Function data pointer
   @retval none
 */
-static void httpStreamEventHandler(struct mg_connection* nc, int ev, void* ev_data, void* user_data)
+static void httpStreamEventHandler(struct mg_connection* nc, int ev, void* ev_data, void* fn_data)
 {
   switch(ev)
   {
-    case MG_EV_HTTP_REQUEST:
+    case MG_EV_HTTP_MSG:
     {
       char addr[32];
-      mg_sock_addr_to_str(&nc->sa, addr, sizeof(addr), MG_SOCK_STRINGIFY_IP | MG_SOCK_STRINGIFY_PORT);
+      mg_straddr(nc, addr, sizeof(addr));
 
       // Lock the client list
       xSemaphoreTake(client_mutex, portMAX_DELAY);
@@ -56,7 +56,7 @@ static void httpStreamEventHandler(struct mg_connection* nc, int ev, void* ev_da
         return;
       }
 
-      nc->user_data = queue;
+      nc->fn_data = queue;
       clients.insert(nc);
 
       // Notify system of first client
@@ -65,32 +65,32 @@ static void httpStreamEventHandler(struct mg_connection* nc, int ev, void* ev_da
 
       xSemaphoreGive(client_mutex);
 
-      // Grab the stream object from the user_data
-      HTTP::StreamConfig* stream_config = (HTTP::StreamConfig*) user_data;
+      // Grab the stream object from the fn_data
+      HTTP::StreamConfig* stream_config = (HTTP::StreamConfig*) fn_data;
       assert(stream_config != nullptr);
 
       ESP_LOGI(TAG, "New %s client %p (%s).", stream_config->name, nc, addr);
 
       // Send the HTTP header
-      mg_send_response_line(nc, 200, stream_config->headers);
+      mg_http_reply(nc, 200, stream_config->headers, "");
 
       // Perform additional setup if needed
       if (stream_config->setup)
         stream_config->setup(nc);
 
       // Reassign event handler for this client
-      nc->handler = httpStreamEventHandler;
+      nc->fn = httpStreamEventHandler;
 
       break;
     }
 
-    case MG_EV_SEND:
+    case MG_EV_WRITE:
     case MG_EV_POLL:
     {
       // Service queues on every poll or send event
 
       // Get queue handle from the connection
-      QueueHandle_t queue = (QueueHandle_t) nc->user_data;
+      QueueHandle_t queue = (QueueHandle_t) nc->fn_data;
       assert(queue != nullptr);
   
       I2S::sample_buffer_t samples;
@@ -103,12 +103,12 @@ static void httpStreamEventHandler(struct mg_connection* nc, int ev, void* ev_da
     case MG_EV_CLOSE:
     {
       char addr[32];
-      mg_sock_addr_to_str(&nc->sa, addr, sizeof(addr), MG_SOCK_STRINGIFY_IP | MG_SOCK_STRINGIFY_PORT);
+      mg_straddr(nc, addr, sizeof(addr));
       ESP_LOGI(TAG, "Client %p (%s) disconnected.", nc, addr);
 
       // Delete the clients queue
-      if (nc->user_data != nullptr)
-        vQueueDelete((QueueHandle_t) nc->user_data);
+      if (nc->fn_data != nullptr)
+        vQueueDelete((QueueHandle_t) nc->fn_data);
       else
         ESP_LOGE(TAG, "No queue for client %p (%s).", nc, addr);
 
@@ -136,30 +136,25 @@ static void httpStreamEventHandler(struct mg_connection* nc, int ev, void* ev_da
   @param  nc Mongoose connection
   @param  ev Mongoose event calling the function
   @param  ev_data Event data pointer
-  @param  user_data User data pointer
+  @param  fn_data Function data pointer
   @retval none
 */
-static void httpEventHandler(struct mg_connection* nc, int ev, void* ev_data, void* user_data)
+static void httpEventHandler(struct mg_connection* nc, int ev, void* ev_data, void* fn_data)
 {
   extern const uint8_t index_html[] asm("_binary_index_html_start");
-  extern const uint8_t index_html_end[] asm("_binary_index_html_end");
-  const uint32_t index_html_len = index_html_end - index_html;
 
   switch(ev)
   {
-    case MG_EV_HTTP_REQUEST:
+    case MG_EV_HTTP_MSG:
     {
-      struct http_message *hm = (struct http_message *) ev_data;
+      struct mg_http_message *hm = (struct mg_http_message*) ev_data;
       
       char action[4];
-      if (mg_get_http_var(&hm->query_string, "action", action, sizeof(action)) == -1)
+      if (mg_http_get_var(&hm->query, "action", action, sizeof(action)) == -1)
       {
-        // Send the header
-        mg_send_head(nc, 200, index_html_len, "Content-Type: text/html");
-
         // Serve the page
-        mg_send(nc, index_html, index_html_len);
-        nc->flags |= MG_F_SEND_AND_CLOSE;
+        mg_http_reply(nc, 200, "Content-Type: text/html\r\n", "%s", index_html);
+        nc->is_draining = true;
         break;
       }
       else if (strcmp(action, "get") == 0) // Get JSON values
@@ -168,16 +163,15 @@ static void httpEventHandler(struct mg_connection* nc, int ev, void* ev_data, vo
 
         ESP_LOGI(TAG, "Get = %s", renderers.c_str());
 
-        mg_send_head(nc, 200, renderers.length(), "Content-Type: application/json");
-        mg_send(nc, renderers.c_str(), renderers.length());
-        nc->flags |= MG_F_SEND_AND_CLOSE;
+        mg_http_reply(nc, 200, "Content-Type: application/json\r\n", "%s", renderers.c_str());
+        nc->is_draining = true;
     
         break;
       }
       else if (strcmp(action, "set") == 0) // Set JSON values
       {
         // Move JSON data into null terminated buffer
-        std::string buffer(hm->body.p, hm->body.p + hm->body.len);
+        std::string buffer(hm->body.ptr, hm->body.ptr + hm->body.len);
 
         ESP_LOGI(TAG, "Set = %s", buffer.c_str());
 
@@ -185,14 +179,13 @@ static void httpEventHandler(struct mg_connection* nc, int ev, void* ev_data, vo
 
         const char* error_string = success ? "Update successful." : "JSON parse failed.";
         
-        mg_send_head(nc, success ? 200 : 400, strlen(error_string), "Content-Type: text/html");
-        mg_printf(nc, error_string);
-        nc->flags |= MG_F_SEND_AND_CLOSE;
+        mg_http_reply(nc, success ? 200 : 400, "Content-Type: text/html\r\n", error_string);
+        nc->is_draining = true;
       }
       else
       {
-        mg_http_send_redirect(nc, 302, hm->uri, mg_mk_str(NULL));
-        nc->flags |= MG_F_SEND_AND_CLOSE;
+        mg_http_reply(nc, 302, "Location: /\r\n", "");
+        nc->is_draining = true;
       }
       break;
     }
@@ -208,176 +201,176 @@ static void httpEventHandler(struct mg_connection* nc, int ev, void* ev_data, vo
   @param  nc Mongoose connection
   @param  ev Mongoose event calling the function
   @param  ev_data Event data pointer
-  @param  user_data User data pointer
+  @param  fn_data Function data pointer
   @retval none
 */
-static void otaEventHandler(struct mg_connection* nc, int ev, void* ev_data, void* user_data)
-{
-  extern const uint8_t ota_html[] asm("_binary_ota_html_start");
-  extern const uint8_t ota_html_end[] asm("_binary_ota_html_end");
-  const uint32_t ota_html_len = ota_html_end - ota_html;
+// static void otaEventHandler(struct mg_connection* nc, int ev, void* ev_data, void* fn_data)
+// {
+//   extern const uint8_t ota_html[] asm("_binary_ota_html_start");
+//   extern const uint8_t ota_html_end[] asm("_binary_ota_html_end");
+//   const uint32_t ota_html_len = ota_html_end - ota_html;
 
-  constexpr uint32_t MG_F_OTA_FAILED = MG_F_USER_1;
-  constexpr uint32_t MG_F_OTA_COMPLETE = MG_F_USER_2;
+//   constexpr uint32_t MG_F_OTA_FAILED = MG_F_USER_1;
+//   constexpr uint32_t MG_F_OTA_COMPLETE = MG_F_USER_2;
 
-  static std::queue<OTA::end_callback_t> callbacks;
-  static std::string response;
+//   static std::queue<OTA::end_callback_t> callbacks;
+//   static std::string response;
 
-  switch(ev)
-  {
-    case MG_EV_HTTP_REQUEST:
-    {
-      // Send the header
-      mg_send_head(nc, 200, ota_html_len, "Content-Type: text/html");
+//   switch(ev)
+//   {
+//     case MG_EV_HTTP_MSG:
+//     {
+//       // Send the header
+//       mg_send_head(nc, 200, ota_html_len, "Content-Type: text/html");
 
-      // Serve the page
-      mg_send(nc, ota_html, ota_html_len);
-      nc->flags |= MG_F_SEND_AND_CLOSE;
-      break;
-    }
+//       // Serve the page
+//       mg_send(nc, ota_html, ota_html_len);
+//       nc->is_draining= true;
+//       break;
+//     }
 
-    case MG_EV_HTTP_MULTIPART_REQUEST:
-    {
-      // Reset response string
-      response.clear();
-      break;
-    }
+//     case MG_EV_HTTP_MULTIPART_REQUEST:
+//     {
+//       // Reset response string
+//       response.clear();
+//       break;
+//     }
 
-    case MG_EV_HTTP_PART_BEGIN:
-    {
-      struct mg_http_multipart_part* multipart = (struct mg_http_multipart_part*) ev_data;
+//     case MG_EV_HTTP_PART_BEGIN:
+//     {
+//       struct mg_http_multipart_part* multipart = (struct mg_http_multipart_part*) ev_data;
 
-      if (multipart->user_data != nullptr)
-      {
-        ESP_LOGE(TAG, "Non-null OTA handle. OTA already in progress?");
+//       if (multipart->fn_data != nullptr)
+//       {
+//         ESP_LOGE(TAG, "Non-null OTA handle. OTA already in progress?");
 
-        // Mark OTA as failed and append reason
-        response += "OTA update already in progress.";
-        nc->flags |= MG_F_OTA_FAILED;
-        return;
-      }
+//         // Mark OTA as failed and append reason
+//         response += "OTA update already in progress.";
+//         nc->flags |= MG_F_OTA_FAILED;
+//         return;
+//       }
 
-      // Construct new OTA handle for application
-      OTA::Handle* ota = new OTA::AppHandle();
-      if (ota == nullptr)
-      {
-        ESP_LOGE(TAG, "Failed to construct OTA handle.");
+//       // Construct new OTA handle for application
+//       OTA::Handle* ota = new OTA::AppHandle();
+//       if (ota == nullptr)
+//       {
+//         ESP_LOGE(TAG, "Failed to construct OTA handle.");
 
-        // Mark OTA as failed and append reason
-        response += "OTA update init failed.";
-        nc->flags |= MG_F_OTA_FAILED;
-        return;
-      }
+//         // Mark OTA as failed and append reason
+//         response += "OTA update init failed.";
+//         nc->flags |= MG_F_OTA_FAILED;
+//         return;
+//       }
 
-      ESP_LOGI(TAG, "Starting OTA...");
-      esp_err_t result = ota->start();
-      if (result != ESP_OK)
-      {
-        ESP_LOGE(TAG, "Failed to start OTA. Error: %s", esp_err_to_name(result));
+//       ESP_LOGI(TAG, "Starting OTA...");
+//       esp_err_t result = ota->start();
+//       if (result != ESP_OK)
+//       {
+//         ESP_LOGE(TAG, "Failed to start OTA. Error: %s", esp_err_to_name(result));
 
-        // Mark OTA as failed and append reason
-        response += "OTA update init failed.";
-        nc->flags |= MG_F_OTA_FAILED;
+//         // Mark OTA as failed and append reason
+//         response += "OTA update init failed.";
+//         nc->flags |= MG_F_OTA_FAILED;
 
-        // Kill the handle
-        delete ota;
-        multipart->user_data = nullptr;
-        return;
-      }
+//         // Kill the handle
+//         delete ota;
+//         multipart->fn_data = nullptr;
+//         return;
+//       }
 
-      // Save the handle for reference in future calls
-      multipart->user_data = (void*) ota;
-      break;
-    }
+//       // Save the handle for reference in future calls
+//       multipart->fn_data = (void*) ota;
+//       break;
+//     }
 
-    case MG_EV_HTTP_PART_DATA:
-    {
-      struct mg_http_multipart_part* multipart = (struct mg_http_multipart_part*) ev_data;
+//     case MG_EV_HTTP_PART_DATA:
+//     {
+//       struct mg_http_multipart_part* multipart = (struct mg_http_multipart_part*) ev_data;
 
-      // Something went wrong so ignore the data
-      if (nc->flags & MG_F_OTA_FAILED)
-        return;
+//       // Something went wrong so ignore the data
+//       if (nc->flags & MG_F_OTA_FAILED)
+//         return;
 
-      // Fetch handle from user_data
-      OTA::Handle* ota = (OTA::Handle*) multipart->user_data;
-      if (ota == nullptr)
-        return;
+//       // Fetch handle from fn_data
+//       OTA::Handle* ota = (OTA::Handle*) multipart->fn_data;
+//       if (ota == nullptr)
+//         return;
 
-      if (ota->write((uint8_t*) multipart->data.p, multipart->data.len) != ESP_OK)
-      {
-        // Mark OTA as failed and append reason
-        response += "OTA write failed.";
-        nc->flags |= MG_F_OTA_FAILED;
-      }
-      break;
-    }
+//       if (ota->write((uint8_t*) multipart->data.p, multipart->data.len) != ESP_OK)
+//       {
+//         // Mark OTA as failed and append reason
+//         response += "OTA write failed.";
+//         nc->flags |= MG_F_OTA_FAILED;
+//       }
+//       break;
+//     }
 
-    case MG_EV_HTTP_PART_END:
-    {
-      struct mg_http_multipart_part* multipart = (struct mg_http_multipart_part*) ev_data;
+//     case MG_EV_HTTP_PART_END:
+//     {
+//       struct mg_http_multipart_part* multipart = (struct mg_http_multipart_part*) ev_data;
       
-      // Fetch handle from user_data
-      OTA::Handle* ota = (OTA::Handle*) multipart->user_data;
-      if (ota == nullptr)
-        return;
+//       // Fetch handle from fn_data
+//       OTA::Handle* ota = (OTA::Handle*) multipart->fn_data;
+//       if (ota == nullptr)
+//         return;
 
-      // Even if MG_F_OTA_FAILED is set we should let the OTA try to clean up
+//       // Even if MG_F_OTA_FAILED is set we should let the OTA try to clean up
 
-      ESP_LOGI(TAG, "Ending OTA...");
-      OTA::end_result_t result = ota->end();
+//       ESP_LOGI(TAG, "Ending OTA...");
+//       OTA::end_result_t result = ota->end();
 
-      // Save callback
-      callbacks.push(result.callback);
+//       // Save callback
+//       callbacks.push(result.callback);
 
-      // Update response if error
-      if (result.status != ESP_OK)
-      {
-        response += "OTA end failed.";
-        nc->flags |= MG_F_OTA_FAILED;
-      }
+//       // Update response if error
+//       if (result.status != ESP_OK)
+//       {
+//         response += "OTA end failed.";
+//         nc->flags |= MG_F_OTA_FAILED;
+//       }
 
-      // Free the handle object and reference
-      delete ota;
-      multipart->user_data = nullptr;
-      break;
-    }
+//       // Free the handle object and reference
+//       delete ota;
+//       multipart->fn_data = nullptr;
+//       break;
+//     }
 
-    case MG_EV_HTTP_MULTIPART_REQUEST_END:
-    {
-      // Send the appropriate reply
-      const char* reply = nc->flags & MG_F_OTA_FAILED ? response.c_str() : "OTA update successful.";
+//     case MG_EV_HTTP_MULTIPART_REQUEST_END:
+//     {
+//       // Send the appropriate reply
+//       const char* reply = nc->flags & MG_F_OTA_FAILED ? response.c_str() : "OTA update successful.";
       
-      mg_send_head(nc, nc->flags & MG_F_OTA_FAILED ? 500 : 200, strlen(reply), "Content-Type: text/html");
-      mg_printf(nc, reply);
-      nc->flags |= MG_F_SEND_AND_CLOSE;
+//       mg_send_head(nc, nc->flags & MG_F_OTA_FAILED ? 500 : 200, strlen(reply), "Content-Type: text/html");
+//       mg_printf(nc, reply);
+//       nc->is_draining = true;
 
-      // Signal OTA is complete
-      nc->flags |= MG_F_OTA_COMPLETE;
+//       // Signal OTA is complete
+//       nc->flags |= MG_F_OTA_COMPLETE;
 
-      response.clear();
-      break;
-    }
+//       response.clear();
+//       break;
+//     }
 
-    case MG_EV_CLOSE:
-    {
-      // Ignore close events that aren't after an OTA
-      if ((nc->flags & MG_F_OTA_COMPLETE) != MG_F_OTA_COMPLETE)
-       return;
+//     case MG_EV_CLOSE:
+//     {
+//       // Ignore close events that aren't after an OTA
+//       if ((nc->flags & MG_F_OTA_COMPLETE) != MG_F_OTA_COMPLETE)
+//        return;
       
-      // Fire callbacks once OTA connection has closed
-      while (callbacks.empty() == false)
-      {
-        OTA::end_callback_t callback = callbacks.front();
-        if (callback)
-          callback();
+//       // Fire callbacks once OTA connection has closed
+//       while (callbacks.empty() == false)
+//       {
+//         OTA::end_callback_t callback = callbacks.front();
+//         if (callback)
+//           callback();
 
-        callbacks.pop();
-      }
+//         callbacks.pop();
+//       }
     
-     break;
-    }
-  }
-}
+//      break;
+//     }
+//   }
+// }
 
 /**
   @brief  Main task function of the HTTP server
@@ -398,10 +391,10 @@ void HTTP::task(void* pvParameters)
 
   // Create and init the event manager
   struct mg_mgr manager;
-  mg_mgr_init(&manager, NULL);
+  mg_mgr_init(&manager);
 
   // Connect bind to an address and specify the event handler
-  struct mg_connection* connection = mg_bind(&manager, "80", httpEventHandler, nullptr);
+  struct mg_connection* connection = mg_http_listen(&manager, "0.0.0.0:80", httpEventHandler, nullptr);
   if (connection == NULL)
   {
     ESP_LOGE(TAG, "Failed to bind port.");
@@ -410,27 +403,24 @@ void HTTP::task(void* pvParameters)
     return;
   }
 
-  // Enable HTTP on the connection
-  mg_set_protocol_http_websocket(connection);
-
   // Special handler for OTA page
-  mg_register_http_endpoint(connection, "/ota", otaEventHandler, nullptr);
+  // mg_register_http_endpoint(connection, "/ota", otaEventHandler, nullptr);
 
-  // Construct the PCM stream object
-  StreamConfig pcm("PCM", "Content-Type: audio/L16;rate=48000;channels=2\r\nAccept-Ranges: none\r\nCache-Control: no-cache,no-store,must-revalidate,max-age=0\r\n");
+  // // Construct the PCM stream object
+  // StreamConfig pcm("PCM", "Content-Type: audio/L16;rate=48000;channels=2\r\nAccept-Ranges: none\r\nCache-Control: no-cache,no-store,must-revalidate,max-age=0\r\n");
 
-  // Construct the WAV stream object
-  StreamConfig wav("WAV", "Content-Type: audio/wav\r\nAccept-Ranges: none\r\nCache-Control: no-cache,no-store,must-revalidate,max-age=0\r\n");
-  wav.setup = [](struct mg_connection* nc)
-  {
-    // Construct and send the WAV header
-    WAV::Header wav_header(48000);
-    mg_send(nc, &wav_header, sizeof(wav_header));
-  };
+  // // Construct the WAV stream object
+  // StreamConfig wav("WAV", "Content-Type: audio/wav\r\nAccept-Ranges: none\r\nCache-Control: no-cache,no-store,must-revalidate,max-age=0\r\n");
+  // wav.setup = [](struct mg_connection* nc)
+  // {
+  //   // Construct and send the WAV header
+  //   WAV::Header wav_header(48000);
+  //   mg_send(nc, &wav_header, sizeof(wav_header));
+  // };
 
   // Add separate end points for raw PCM and WAV stream
-  mg_register_http_endpoint(connection, "/stream.pcm", httpStreamEventHandler, &pcm);
-  mg_register_http_endpoint(connection, "/stream.wav", httpStreamEventHandler, &wav);
+  // mg_register_http_endpoint(connection, "/stream.pcm", httpStreamEventHandler, &pcm);
+  // mg_register_http_endpoint(connection, "/stream.wav", httpStreamEventHandler, &wav);
 
   // Loop waiting for events
   while(1)
@@ -460,7 +450,7 @@ void HTTP::queue_samples(const I2S::sample_buffer_t& samples)
   for (const auto nc : clients)
   {
     // Get queue handle from the connection
-    QueueHandle_t queue = (QueueHandle_t) nc->user_data;
+    QueueHandle_t queue = (QueueHandle_t) nc->fn_data;
     assert(queue != nullptr);
 
     // Attempt to queue the sample data
